b571ba2c0b0123fc15b460070071d45eeee8514c
chris <2002chanv@gmail.com>
2025-09-27 21:59:12 +0800

    hello debug

--- src/router/index.js
+++ src/router/index.js
@@ -9,9 +9,6 @@ import RegisterFarmerView from '../views/RegisterFarmerView.vue'
 import RegisterPublicView from '../views/RegisterPublicView.vue'
 import RegisterAdminView from '../views/RegisterAdminView.vue'
 
-// Icons
-import '@fortawesome/fontawesome-free/css/all.min.css'
-
 const routes = [
   {
     path: '/',
